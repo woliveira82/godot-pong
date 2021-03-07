cddd8dbb60730e86dfd07a41fa14cbc6621b4124
Weiner Oliveira <woliveira82@gmail.com>
2021-03-07 00:25:49 -0300

    Final adjustments

--- Level.gd
+++ Level.gd
@@ -5,6 +5,12 @@ var player_score = 0
 var opponent_score = 0
 
 
+func _ready():
+	get_tree().call_group('BallGroup', 'stop_ball')
+	$CountdownTimer.start()
+	$CountDown.visible = true
+	
+	
 func _on_Left_body_entered(body: Node):
 	opponent_score += 1
 	score_achieved()
@@ -18,14 +24,20 @@ func _on_Right_body_entered(body: Node):
 func _process(delta: float):
 	$PlayerScore.text = str(player_score)
 	$OpponentScore.text = str(opponent_score)
+	$CountDown.text = str(int($CountdownTimer.time_left) + 1)
 
 
 func _on_CountdownTimer_timeout():
 	get_tree().call_group('BallGroup', 'restart_ball')
 	$CountdownTimer.stop()
+	$CountDown.visible = false
 	
 	
 func score_achieved():
 	$Ball.position = Vector2(640, 360)
 	get_tree().call_group('BallGroup', 'stop_ball')
 	$CountdownTimer.start()
+	$CountDown.visible = true
+	$ScoreSound.play()
+	$Player.position = Vector2(50, 360)
+	$Opponent.position = Vector2(1230, 360)
